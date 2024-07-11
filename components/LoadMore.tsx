88afe6d4c2372d4ca783ec0fd325b2161c37b357
Mohamed Yansane <yansane472@gmail.com>
2024-07-11 00:13:30 +0000

    creating a animecard type

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,15 +4,20 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 
 let page = 2; // we gonna update it each time we get a new view
-
+export type AnimeCard = JSX.Element;
 function LoadMore() {
   // the react intersection observer gonna tell us whwn we reaach a specific
   //point that gonna help us to render the next page
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  // after changing the server i'm getting Type 'AnimeProp[]' is not assignable to type 'ReactNode'.
+  //Type 'AnimeProp[]' is not assignable to type 'Iterable<ReactNode>'.
+  //The types returned by '[Symbol.iterator]().next(...)' are incompatible between these types.
+
+  //* to resolve it i'm gonna create a type of jsxElement instead of aniprop im gonna call it anicard above
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
@@ -33,9 +38,7 @@ function LoadMore() {
     <>
       {/**we copy the entire code from the home page the code i paste is for all the subsequent for page 1 */}
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         {/** now we r giving a ref to our div not it gonna know */}
